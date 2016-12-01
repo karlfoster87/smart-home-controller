58ad29ef4af2287167e146c3dad5c124915b13f8
karlfoster87 <karlfoster87@gmail.com>
2016-12-01 21:46:45 +0000

    Update server.py

--- server.py
+++ server.py
@@ -1,16 +1,32 @@
 from flask import Flask, render_template
 from flask_ask import Ask, statement, convert_errors
+from phue import Bridge
+import ssl
 
 app = Flask(__name__)
 ask = Ask(app, '/alexa/')
 
+global_huebridgeip = '192.168.0.0'
+global_away = False
+global_jodieaway = False
+global_karlaway = False
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+	return render_template('index.html')
 
 @ask.intent('SmartHomeIntent', mapping={'device': 'device', 'mode': 'mode'})
 def smart_home(device, mode):
-	return statement('Message Recieved! {} / {}'.format(device, mode))
+	lighting_api()
+	return statement('Success')
+
+def lighting_api():
+	b = Bridge(global_huebridgeip)
+	b.connect()
+	b.set_light('Floor Lamp', 'bri', 1)
+
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=1337)
+	context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+	context.load_cert_chain('tls.crt', 'tls.key')
+	app.run(host='0.0.0.0', port=1337, ssl_context=context, threaded=True, debug=True)
